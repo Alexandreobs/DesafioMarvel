9e546b20715404d970b19d00a14e30df558d1342
Alexandreobs <53497043+Alexandreobs@users.noreply.github.com>
2019-11-11 17:19:42 -0300

    avanço antes da partre logica

---
+++ app/src/main/java/com/example/desafiomarvel/model/data/remote/MarvelAPI.java
@@ -0,0 +1,15 @@
+package com.example.desafiomarvel.model.data.remote;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MarvelAPI {
+
+  /*  @GET(​"comics?"​)Single<ComicsResponse> getComics(@Query(​"format"​) String format,
+                                                      @Query(​"formatType"​) String formatType,
+                                                      @Query(​"noVariants"​) ​boolean ​noVariants,
+                                                      @Query(​"orderBy"​) String orderBy,
+                                                      @Query(​"ts"​) String ts,
+                                                      @Query(​"hash"​) String hash,
+                                                      @Query(​"apikey"​) String apikey);
+*/}
--- app/src/main/java/com/example/desafiomarvel/view/activity/MainActivity.java
+++ app/src/main/java/com/example/desafiomarvel/view/activity/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.desafiomarvel.view.activity;
 import android.os.Bundle;
 
 import com.example.desafiomarvel.R;
+import com.example.desafiomarvel.view.fragment.recycler.AutoresFragment;
+import com.example.desafiomarvel.view.fragment.recycler.HeroisFragment;
+import com.example.desafiomarvel.view.fragment.recycler.QuadrinhosFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,14 +24,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_Heroi, R.id.navigation_Quadrinhos, R.id.navigation_Autores)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        navView.setOnNavigationItemSelectedListener(menuItem ->{
+            int id =menuItem.getItemId();
+
+            if(id == R.id.navigation_Heroi){
+                replaceFragment(new HeroisFragment());
+
+            } else if (id == R.id.navigation_Quadrinhos){
+
+                replaceFragment(new QuadrinhosFragment());
+
+            } else if (id == R.id.navigation_Autores){
+
+                replaceFragment(new AutoresFragment());
+            }
+
+            return true;
+        });
+
     }
 
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.containerPrincipal, fragment);
+        transaction.commit();
+
+    }
 }
